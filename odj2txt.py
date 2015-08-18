5a673f8082a2e5ee551ff6973f61652db7b0b464
Données ouvertes 101 <pascal.robichaud.do101@gmail.com>
2015-08-18 16:53:18 -0400

    Update odj2txt.py

--- odj2txt.py
+++ odj2txt.py
@@ -6,7 +6,7 @@
 Convertit la section 20 - Affaires contractuelles du fichier PDF 
 de l'ordre du jour en format texte.
 
-Version 1.0, 2015-08-18
+Version 1.1, 2015-08-18
 Développé en Python 3.4
 Licence CC-BY-NC 4.0 Pascal Robichaud, pascal.robichaud.do101@gmail.com
 """
@@ -28,10 +28,16 @@ def main():
 
     # Répertoire où les fichiers PDF sont enregistrés
     REPERTOIRE_PDF = "C:\\ContratsOuvertsMtl\\Ordres_du_jour\\PDF"
+    
+    if not os.path.exists(REPERTOIRE_PDF):
+        raise ValueError("Le repertoire " + REPERTOIRE_PDF + " pour les fichier PDF n'existe pas.")
 
     # Répertoire où le fichier texte résultant sera sauvegardé
     REPERTOIRE_TXT = "C:\\ContratsOuvertsMtl\\Ordres_du_jour\\TXT"
-
+    
+    if not os.path.exists(REPERTOIRE_TXT):
+        os.makedirs(REPERTOIRE_TXT)
+    
     for filename in os.listdir(REPERTOIRE_PDF):
         fichier_PDF = os.path.join(REPERTOIRE_PDF, filename)
         print("Traitement du fichier %s" % fichier_PDF)
@@ -50,9 +56,9 @@ def transforme_pdf_en_txt(fichier_PDF, REPERTOIRE_TXT):
     TITRE_SECTION_30 = " Administration et finances"
 
     prefixe_txt = os.path.splitext(os.path.basename(fichier_PDF))[0]
+    fichier_TXT_temp = os.path.join(REPERTOIRE_TXT, prefixe_txt + '_temp.txt')
     fichier_TXT = os.path.join(REPERTOIRE_TXT, prefixe_txt + '.txt')
     odj_traites = open(fichier_TXT, "w")
-    fodj_traites = csv.writer(odj_traites, delimiter=';')
 
     est_dans_section_affaires_contractuelles = False
     est_dans_section_suivante = False
@@ -62,26 +68,19 @@ def transforme_pdf_en_txt(fichier_PDF, REPERTOIRE_TXT):
         compteur_page += 1
 
         print("Traitement de la page %s" % compteur_page)
-
-        nom_du_fichier_page = os.path.join(
-            REPERTOIRE_TXT,
-            '%s_page_%d.txt' % (prefixe_txt, compteur_page))
-            
+        
         args = [
             'pdf2txt',
             '-p', str(compteur_page),
-            '-o', nom_du_fichier_page,
+            '-o', fichier_TXT_temp,
             fichier_PDF,
         ]
         
         pdf2txt.main(args)
 
-        with open(nom_du_fichier_page, 'r') as f:
-            # Pourquoi du CSV? Tu peux lire les lignes directement de
-            # l'objet retourné par open().
-            reader = csv.reader(f, delimiter='|')
-
-            for ligne in reader:
+        with open(fichier_TXT_temp, 'r') as f:
+            
+            for ligne in f:
 
                 if not est_dans_section_affaires_contractuelles:
                     if TITRE_SECTION_20 in ligne:
@@ -90,15 +89,18 @@ def transforme_pdf_en_txt(fichier_PDF, REPERTOIRE_TXT):
                 if TITRE_SECTION_30 in ligne:
                     est_dans_section_suivante = True
                     break
+                    
                 elif est_dans_section_affaires_contractuelles:
 
                     if ligne.startswith("['Page "):
-                        # Ne pas écrire le numéro de page du pied-de-page.
+                        # Ne pas écrire le numéro de page du pied-de-page
                         break
                     else:
-                        # Ajouter la ligne dans le fichier fichier_TXT.
-                        fodj_traites.writerow(ligne)
-
+                        # Ajouter la ligne dans le fichier fichier_TXT
+                        odj_traites.writelines(ligne)
+    
+    os.remove(fichier_TXT_temp)
+                        
     odj_traites.close()
 
 if __name__ == '__main__':
